6845f706cb24cde739f23deaee1100302af3cc03
mackcyl <mackchen@hotmail.com>
2015-01-09 18:04:14 +0800

    更新首页广告牌
    更新首页广告牌, 使用跨域请求服务器动态获取;

--- controller/index.controller.js
+++ controller/index.controller.js
@@ -1,16 +1,16 @@
-var apiUrl="http://kaola.tieson.cn/NativeApi/";
+//var apiUrl="http://kaola.tieson.cn/NativeApi/";
+var apiUrl="http://172.16.1.241:8888/koalamall_3.2/NativeApi/"
 var xhr=null;
-
-function loadSeckillProduct() {
-	if ( xhr ) {
-		outLine( "xhr请求已创建" );
+/*
+function loadIndexBillboard(){
+	if(xhr){
 		return;
 	}
 	outSet( "创建请求：" );
 	xhr = new plus.net.XMLHttpRequest();
-	xhr.onreadystatechange = function () {
-        switch ( xhr.readyState ) {
-            case 0:
+	xhr.onreadystatechange = function(){
+		switch (xhr.readyState){
+			 case 0:
             	outLine( "xhr请求已初始化" );
             break;
             case 1:
@@ -22,29 +22,145 @@ function loadSeckillProduct() {
             case 3:
                 outLine( "xhr请求已响应");
                 break;
-            case 4:
-                outLine( "xhr请求已完成");
-                if ( xhr.status == 200 ) {
-                		var ResponsdObj = JSON.parse(xhr.responseText);
-                		 	outLine( "API主机："+apiUrl);
+			case 4:{
+				 if ( xhr.status == 200 ) {
+				 		outLine( "API主机："+apiUrl);
                 		 	outLine( "API接口：Index/loadSecKillList")
                 		 	outLine( "请求方式："+"GET");
-                		 	
+                		var ResponsdObj = JSON.parse(xhr.responseText);
+                		var sliderOne = "";
+                		var sliderTwo = "";
+                		var sliderBody = "";
+                		var sliderRight = "<div class=\"mui-slider-indicator mui-text-right\">";
                 		for (i=0;i<ResponsdObj.length; i++) {
-                			outLine( "xhr请求成功：id="+ResponsdObj[i].id+" name="+ResponsdObj[i].name+" price="+ResponsdObj[i].price);        
+                			if(0==i){
+                				sliderOne += "<div class=\"mui-slider-item mui-slider-item-duplicate\">"
+										    	"<a href=\""+ResponsdObj[i].billboard_link+"\">""
+										    		"<img src=\""+ResponsdObj[i].image_src+"\" height=\"200px;\" />"
+										    		"<p class=\"mui-slider-title\">"+ResponsdObj[i].billboard_title+"</p>";
+										    	"</a>"
+										  "</div>";
+							sliderRight += "<div class=\"mui-indicator mui-active\"></div>";
+							continue;
+                			}
+                			sliderRight += "<div class=\"mui-indicator\"></div>";
+                			if(1==i){
+                				sliderTwo += "<div class=\"mui-slider-item\">"
+										    	"<a href=\""+ResponsdObj[i].billboard_link+"\">""
+										    		"<img src=\""+ResponsdObj[i].image_src+"\" height=\"200px;\" />"
+										    		"<p class=\"mui-slider-title\">"+ResponsdObj[i].billboard_title+"</p>";
+										    	"</a>"
+										  "</div>";
+							continue;
+                			}
+                			sliderBody += "<div class=\"mui-slider-item\">"
+									    	"<a href=\""+ResponsdObj[i].billboard_link+"\">""
+									    		"<img src=\""+ResponsdObj[i].image_src+"\" height=\"200px;\" />"
+									    		"<p class=\"mui-slider-title\">"+ResponsdObj[i].billboard_title+"</p>";
+									    	"</a>"
+									  "</div>";
                 		}
-               
+                		sliderRight += "</div>";
+                		document.getElementById("slider").innerHTML(" <div class=\"mui-slider-group mui-slider-loop\" >"+sliderOne+sliderTwo+sliderBody+sliderTwo+sliderOne+"</div>"+sliderRight);
+//              		var gallery = mui(".mui-slider");
+//			  		gallery.slider({
+//			  			interval:10000
+//			  		});
                 } else {
-                	outLine( "xhr请求失败："+xhr.status );
+                }
+                break;
+            default :
+                break;
+			}
+		}
+	}
+	outLine(apiUrl+"Index/loadIndexBillboard");
+	xhr.open( "POST", apiUrl+"Index/loadIndexBillboard" );
+	xhr.send();
+}
+*/
+function loadIndexBillboard() {
+	
+	if ( xhr ) {
+		return;
+	}
+	xhr = new plus.net.XMLHttpRequest();
+	xhr.onreadystatechange = function () {
+        switch ( xhr.readyState ) {
+            case 0:
+            break;
+            case 1:
+            break;
+            case 2:
+            break;
+            case 3:
+                break;
+            case 4:
+                if ( xhr.status == 200 ) {
+                		var ResponsdObj = JSON.parse(xhr.responseText);
+                		var sliderOne = "";
+                		var sliderTwo = "";
+                		var sliderBody = "";
+                		var sliderRight = "<div class=\"mui-slider-indicator mui-text-right\">";
+                		for (i=0;i<ResponsdObj.length; i++) {
+                			if(0==i){
+                				sliderOne += "<div class=\"mui-slider-item mui-slider-item-duplicate\">"
+										  +"<a href=\""+ResponsdObj[i].billboard_link+"\">"
+										  +	"<img src=\""+ResponsdObj[i].image_src+"\" height=\"200px;\" />"
+										  +	"<p class=\"mui-slider-title\">"+ResponsdObj[i].billboard_title+"</p>"
+										  +"</a>"
+										  +"</div>";
+							sliderRight += "<div class=\"mui-indicator mui-active\"></div>";
+							continue;
+                			}
+                			sliderRight += "<div class=\"mui-indicator\"></div>";
+                			if(1==i){
+                				sliderTwo += "<div class=\"mui-slider-item\">"
+										    	+"<a href=\""+ResponsdObj[i].billboard_link+"\">"
+										    	+"<img src=\""+ResponsdObj[i].image_src+"\" height=\"200px;\" />"
+										    	+"<p class=\"mui-slider-title\">"+ResponsdObj[i].billboard_title+"</p>"
+										    	+"</a>"
+										  +"</div>";
+							continue;
+                			}
+                			sliderBody += "<div class=\"mui-slider-item\">"
+									    	+"<a href=\""+ResponsdObj[i].billboard_link+"\">"
+									    +"<img src=\""+ResponsdObj[i].image_src+"\" height=\"200px;\" />"
+									    	+"<p class=\"mui-slider-title\">"+ResponsdObj[i].billboard_title+"</p>"
+									    +"</a>"
+									  +"</div>";
+                		}
+                		sliderRight += "</div>";
+					var silderDiv = "<div class=\"mui-slider-group mui-slider-loop\" >"
+									+sliderOne
+									+sliderTwo
+									+sliderBody
+									+sliderOne
+									+sliderTwo
+									+"</div>"
+									+sliderRight;
+					$("#slider").html(silderDiv);
+					
+					var gallery = mui(".mui-slider");
+			  		gallery.slider({
+			  			interval:10000
+			  		});
+									
+//					document.getElementById("slider").innerHTML(silderDiv);
+//					var mui(".mui-slider").slider({interval:10000});
+//					outLine('123');
+                } else {
+                	 outLine('加载出错');
                 }
                 break;
             default :
                 break;
         }
 	}
-	xhr.open( "GET", apiUrl+"Index/loadSecKillList" );
+	xhr.open( "GET", apiUrl+"Index/loadIndexBillboard" );
 	xhr.send();
 }
+
 function xhrResponseHeader() {
 	if ( xhr ) {
 		if ( xhr.readyState != 4 ) {
@@ -70,6 +186,7 @@ function xhrAbort() {
 		outSet( "未发送请求" );
 	}
 }
+
 var _dout_=null
 _dout_=document.getElementById("output");
 // 输出内容
